d4abe0519754d8abb694190abab995f26407581f
ansitha <ansithamol@gmail.com>
2023-05-20 13:18:23 +0400

    added dashboard structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +22,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Dashboard/>
     </div>
   );
 }
